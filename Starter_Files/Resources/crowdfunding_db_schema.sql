7a7a96883cb5a7fa18459d3af28bfa4706e6b1b7
CassiaY <137340466+CassiaY@users.noreply.github.com>
2023-09-20 21:14:45 -0500

    schema update

--- Starter_Files/Resources/crowdfunding_db_schema.sql
+++ Starter_Files/Resources/crowdfunding_db_schema.sql
@@ -1,56 +1,54 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+--drop if exists
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS subcategory;
+DROP TABLE IF EXISTS campaign;
+DROP TABLE IF EXISTS contacts;
 
-CREATE TABLE "Category" (
-    "category_id" VARCHAR   NOT NULL,
-    "category" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Category" PRIMARY KEY (
-        "category_id"
-     )
+-- Create table 1: category
+CREATE TABLE category (
+  category_id VARCHAR(30) NOT NULL PRIMARY KEY,
+  category VARCHAR(30) NOT NULL,
 );
 
-CREATE TABLE "Subcategory" (
-    "subcategory_id" VARCHAR   NOT NULL,
-    "subcategory" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
-        "subcategory_id"
-     )
+-- Create table 2: subcategory
+CREATE TABLE subcategory (
+    subcategory_id VARCHAR(30) NOT NULL PRIMARY KEY,
+    subcategory VARCHAR(30) NOT NULL,
 );
 
-CREATE TABLE "Contacts" (
-    "contact_id" VARCHAR   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "email" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Contacts" PRIMARY KEY (
-        "contact_id"
-     )
+-- Create table 3: contacts
+CREATE TABLE contacts (
+    contact_id VARCHAR(30) NOT NULL PRIMARY KEY,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    email VARCHAR   NOT NULL,
 );
 
-CREATE TABLE "Campaign" (
-    "cf_id" VARCHAR   NOT NULL,
-    "contact_id" VARCHAR   NOT NULL,
-    "company_name" VARCHAR   NOT NULL,
-    "description" VARCHAR   NOT NULL,
-    "goal" FLOAT   NOT NULL,
-    "pledged" FLOAT   NOT NULL,
-    "outcome" VARCHAR   NOT NULL,
-    "backers_count" INTEGER   NOT NULL,
-    "country" VARCHAR   NOT NULL,
-    "currency" VARCHAR   NOT NULL,
-    "launch_date" DATE   NOT NULL,
-    "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR   NOT NULL,
-    "subcategory_id" VARCHAR   NOT NULL
+-- Create table 4: campaign
+CREATE TABLE campaign (
+    cf_id VARCHAR(30) NOT NULL,
+    contact_id VARCHAR(30) NOT NULL,
+    company_name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    goal FLOAT   NOT NULL,
+    pledged FLOAT   NOT NULL,
+    outcome VARCHAR   NOT NULL,
+    backers_count INTEGER   NOT NULL,
+    country VARCHAR   NOT NULL,
+    currency VARCHAR   NOT NULL,
+    launch_date DATE   NOT NULL,
+    end_date DATE   NOT NULL,
+    category_id VARCHAR   NOT NULL,
+    subcategory_id VARCHAR   NOT NULL,
+        FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+        FOREIGN KEY (category_id) REFERENCES category(category_id),
+        FOREIGN KEY (category_id) REFERENCES subcategory(category_id),
 );
 
-ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "Contacts" ("contact_id");
-
-ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
-REFERENCES "Category" ("category_id");
-
-ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "Subcategory" ("subcategory_id");
-
+SELECT * FROM category;
+SELECT * FROM subcategory;
+SELECT * FROM contacts;
+SELECT * FROM campaign;
